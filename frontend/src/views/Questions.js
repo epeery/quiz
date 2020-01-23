c7562ea70ff94b75a90ae8ec0ad338b49c634a0c
Eli Peery <eli.g.peery@gmail.com>
2020-01-23 16:39:41 -0500

    Added tutorial

--- frontend/src/views/Questions.js
+++ frontend/src/views/Questions.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import Tour from 'reactour'
 
 import { useMachine } from '@xstate/react';
 import { quizMachine, idToString, encodeAnswers } from '../components/quizMachine.js'
@@ -14,6 +16,95 @@ function Questions({data, onBack, onResults}) {
     },
   });
 
+  const [open, setOpen] = useState(true);
+
+  const steps = [
+    {
+      content: ({goTo}) => (
+        <div className='tutorial'>
+          <h2>Welcome!</h2>
+          <h3>Would you like a quick tutorial?</h3>
+          <button onClick={() => goTo(1)} className='opinion-button' data-color='2'>
+            <p>Ok!</p>
+          </button>
+        </div>
+      ),
+    },
+    {
+      content: ({goTo}) => (
+        <div className='tutorial'>
+          <h2>This is an opinion</h2>
+          <h3>You'll be shown 29 of these in total</h3>
+          <button onClick={() => goTo(2)} className='opinion-button' data-color='2'>
+            <p>Next</p>
+          </button>
+        </div>
+      ),
+      selector: '.question'
+    },
+    {
+      content: ({goTo}) => (
+        <div className='tutorial'>
+          <h2>These are stances</h2>
+          <h3>You can select the one that reflects how you feel about the opinion</h3>
+
+          <button onClick={() => goTo(3)} className='opinion-button' data-color='2'>
+            <p>Next</p>
+          </button>
+        </div>
+      ),
+      selector: '.opinion-container'
+    },
+    {
+      content: ({goTo}) => (
+        <div className='tutorial'>
+          <h2>You can click the opinion for more information</h2>
+
+          <button onClick={() => goTo(4)} className='opinion-button' data-color='2'>
+            <p>Next</p>
+          </button>
+        </div>
+      ),
+      selector: '.question',
+    },
+    {
+      content: ({goTo}) => (
+        <div className='tutorial'>
+          <h2>If you still aren't sure, you can always skip the question</h2>
+
+          <button onClick={() => goTo(5)} className='opinion-button' data-color='2'>
+            <p>Next</p>
+          </button>
+        </div>
+      ),
+      selector: '.skip',
+    },
+    {
+      content: ({goTo}) => (
+        <div className='tutorial'>
+          <h2>You can also go back if you'd like to change a previous response</h2>
+
+          <button onClick={() => goTo(6)} className='opinion-button' data-color='2'>
+            <p>Next</p>
+          </button>
+        </div>
+      ),
+      selector: '.back',
+    },
+    {
+      content: ({goTo}) => (
+        <div className='tutorial'>
+          <h2>That's it!</h2>
+          <h3>You'll see a ranking of the democratic candidates once you're done</h3>
+
+          <button onClick={() => setOpen(false)} className='opinion-button' data-color='2'>
+            <p>Great!</p>
+          </button>
+        </div>
+      ),
+    },
+  ]
+
   const questionNumber = current.context.questionNumber;
 
   const getSelected = () => (
@@ -25,49 +116,59 @@ function Questions({data, onBack, onResults}) {
   const backClass = () => (questionNumber === 0 && current.value !== 'info') ? 'back dimmed' : 'back'
 
   return (
-    <div className='Questions'>
-      <nav>
-        <div onClick={() => send('BACK')} className={backClass()} >
-          <div className="icon-container">
-            <svg width='100%' height='100%' viewBox='0 0 14 12' fill='none' xmlns='http://www.w3.org/2000/svg'>
-              <path d='M13 6H1M1 6L6.14286 1M1 6L6.14286 11' stroke='#31456A' strokeWidth='2' strokeLinecap='round' strokeLinejoin='round'/>
-            </svg>
-          </div>
-          <p>Back</p>
-        </div>
-
-          {current.value === 'active' && (
-            <div onClick={() => send('SKIP')} className='skip'>
-              <p>{current.context.answers.hasOwnProperty(idToString(data[questionNumber].qId)) ? 'Next' : 'Skip' }</p>
-              <div className="icon-container">
-                <svg width='100%' height='100%' viewBox='0 0 14 12' fill='none' xmlns='http://www.w3.org/2000/svg'>
-                  <path d='M13 6H1M1 6L6.14286 1M1 6L6.14286 11' stroke='#31456A' strokeWidth='2' strokeLinecap='round' strokeLinejoin='round'/>
-                </svg>
-              </div>
+    <>
+      <Tour
+        steps={steps}
+        showNavigation={false}
+        showButtons={false}
+        rounded={8}
+        isOpen={open}
+        disableInteraction={true}
+        closeWithMask={false}
+        onRequestClose={() => setOpen(false)} />
+      <div className='Questions'>
+        <nav>
+          <div onClick={() => send('BACK')} className={backClass()} >
+            <div className="icon-container">
+              <svg width='100%' height='100%' viewBox='0 0 14 12' fill='none' xmlns='http://www.w3.org/2000/svg'>
+                <path d='M13 6H1M1 6L6.14286 1M1 6L6.14286 11' stroke='#31456A' strokeWidth='2' strokeLinecap='round' strokeLinejoin='round'/>
+              </svg>
             </div>
-          )}
+            <p>Back</p>
+          </div>
 
-      </nav>
+            {current.value === 'active' && (
+              <div onClick={() => send('SKIP')} className='skip'>
+                <p>{current.context.answers.hasOwnProperty(idToString(data[questionNumber].qId)) ? 'Next' : 'Skip' }</p>
+                <div className="icon-container">
+                  <svg width='100%' height='100%' viewBox='0 0 14 12' fill='none' xmlns='http://www.w3.org/2000/svg'>
+                    <path d='M13 6H1M1 6L6.14286 1M1 6L6.14286 11' stroke='#31456A' strokeWidth='2' strokeLinecap='round' strokeLinejoin='round'/>
+                  </svg>
+                </div>
+              </div>
+            )}
 
-      {current.value === 'active' && (
-        <Question number={questionNumber + 1} topic={data[questionNumber].questionTopic} selected={getSelected()}>
-          {data[questionNumber].question}
-        </Question>
-      )}
-      {current.value === 'info' && (
-        <Info question={data[questionNumber]}>
-        </Info>
-      )}
+        </nav>
 
-    </div>
+        {current.value === 'active' && (
+          <Question number={questionNumber + 1} topic={data[questionNumber].questionTopic} selected={getSelected()}>
+            {data[questionNumber].question}
+          </Question>
+        )}
+        {current.value === 'info' && (
+          <Info question={data[questionNumber]}>
+          </Info>
+        )}
+
+      </div>
+    </>
   );
-
 
 
   function Question({children, number, topic, selected}) {
     const buttonClass = num => selected === num ? 'opinion-button selected' : 'opinion-button'
     return (
-      <>
+      <div className='question-container'>
         <h3>Question {number} - {topic}</h3>
         <h2 className='question'><span onClick={() => send('INFO')}>{children}</span></h2>
         <div className='opinion-container'>
@@ -91,20 +192,20 @@ function Questions({data, onBack, onResults}) {
             <p>Strongly Disagree</p>
           </button>
         </div>
-      </>
-    );
-  }
-
-  function Info({question}) {
-    console.log(question);
-    return (
-      <div className='info'>
-        <h2 className='header'>{question.header}</h2>
-        <p className='information'>“{question.info}”</p>
-        <a className='source' href={question.source} target='_blank'><div>{question.source}</div></a>
       </div>
     );
   }
+
+}
+
+function Info({question}) {
+  return (
+    <div className='info'>
+      <h2 className='header'>{question.header}</h2>
+      <p className='information'>“{question.info}”</p>
+      <a className='source' href={question.source} target='_blank'><div>{question.source}</div></a>
+    </div>
+  );
 }
 
 export default Questions;
