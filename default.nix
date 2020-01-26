a95e40112b1aeec91091dfb6ba0640418f3352d8
Eli Peery <eli.g.peery@gmail.com>
2020-01-26 15:00:40 -0500

    Added wai-cors package

--- default.nix
+++ default.nix
@@ -1,6 +1,6 @@
 { mkDerivation, aeson, base, bytestring, containers, hpack, mtl
 , optparse-generic, polysemy, polysemy-plugin, polysemy-zoo
-, servant, servant-server, stdenv, text, warp
+, servant, servant-server, stdenv, text, wai-cors, warp
 }:
 mkDerivation {
   pname = "quiz";
@@ -10,12 +10,14 @@ mkDerivation {
   isExecutable = true;
   libraryHaskellDepends = [
     aeson base bytestring containers mtl optparse-generic polysemy
-    polysemy-plugin polysemy-zoo servant servant-server text warp
+    polysemy-plugin polysemy-zoo servant servant-server text wai-cors
+    warp
   ];
   libraryToolDepends = [ hpack ];
   executableHaskellDepends = [
     aeson base bytestring containers mtl optparse-generic polysemy
-    polysemy-plugin polysemy-zoo servant servant-server text warp
+    polysemy-plugin polysemy-zoo servant servant-server text wai-cors
+    warp
   ];
   prePatch = "hpack";
   license = stdenv.lib.licenses.bsd3;
