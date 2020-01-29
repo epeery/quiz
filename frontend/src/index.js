3ddb27abaf3233efe8450e3aa35852a3fd559507
Eli Peery <eli.g.peery@gmail.com>
2020-01-29 09:17:20 -0500

    Added better way to deal with vh units on mobile

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,12 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+let vh = window.innerHeight * 0.01;
+
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
