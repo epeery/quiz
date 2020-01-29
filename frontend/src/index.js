078bd1468c385727e245815e04000453c111f138
Eli Peery <eli.g.peery@gmail.com>
2020-01-29 11:29:28 -0500

    Added event listener

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,11 +4,11 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
-let vh = window.innerHeight * 0.01;
 
-// Then we set the value in the --vh custom property to the root of the document
-document.documentElement.style.setProperty('--vh', `${vh}px`);
+window.addEventListener('resize', () => {
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
