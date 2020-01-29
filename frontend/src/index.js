1ae006775c404b7f9e02d38875a113f0212da813
Eli Peery <eli.g.peery@gmail.com>
2020-01-29 12:58:32 -0500

    Fixed vh bug

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,11 +5,15 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 
-let vh = window.innerHeight * 0.01;
+const setVh = () => {
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+}
+
+setVh();
 
 window.addEventListener('resize', () => {
-  vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty('--vh', `${vh}px`);
+  setVh();
 });
 
 ReactDOM.render(<App />, document.getElementById('root'));
