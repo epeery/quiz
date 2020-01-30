f2f9d9e2cda5d624e031ef7edde5da6644e4458b
Eli Peery <eli.g.peery@gmail.com>
2020-01-30 12:38:13 -0500

    Added notice that we don't collect data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './styles/main.scss'
+
+import Tour from 'reactour'
 
 import Start from './views/Start.js'
 
 function App() {
+  const [open, setOpen] = useState(true);
+
+  const steps = [
+    {
+      content: ({goTo}) => (
+        <div>
+          <h2>We don't collect any data</h2>
+          <h3>We're also open source! <br/>
+            <a href='https://github.com/epeery/quiz' target='_blank' rel='noopener noreferrer'>Contribute here</a>
+          </h3>
+        </div>
+      ),
+    },
+  ]
+
   return (
-    <div className="App">
-      <Start />
-    </div>
+    <>
+      <Tour
+        steps={steps}
+        showNavigation={false}
+        showButtons={false}
+        rounded={8}
+        isOpen={open}
+        disableInteraction={true}
+        onRequestClose={() => setOpen(false)}
+        />
+
+      <div className="App">
+        <Start />
+      </div>
+    </>
   );
 }
 
