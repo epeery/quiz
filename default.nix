85dcac9b8631733984342c7b82fb122c85126eb0
Eli Peery <eli.g.peery@gmail.com>
2020-01-31 16:29:38 -0500

    Added blaze-html dependency

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
-{ mkDerivation, aeson, base, bytestring, containers, hpack, mtl
-, optparse-generic, polysemy, polysemy-plugin, polysemy-zoo
-, servant, servant-server, stdenv, text, wai, wai-cors, wai-extra
-, warp
+{ mkDerivation, aeson, base, blaze-html, bytestring, containers
+, hpack, mtl, optparse-generic, polysemy, polysemy-plugin
+, polysemy-zoo, servant, servant-server, stdenv, text, wai
+, wai-cors, wai-extra, warp
 }:
 mkDerivation {
   pname = "quiz";
@@ -10,15 +10,15 @@ mkDerivation {
   isLibrary = true;
   isExecutable = true;
   libraryHaskellDepends = [
-    aeson base bytestring containers mtl optparse-generic polysemy
-    polysemy-plugin polysemy-zoo servant servant-server text wai
-    wai-cors wai-extra warp
+    aeson base blaze-html bytestring containers mtl optparse-generic
+    polysemy polysemy-plugin polysemy-zoo servant servant-server text
+    wai wai-cors wai-extra warp
   ];
   libraryToolDepends = [ hpack ];
   executableHaskellDepends = [
-    aeson base bytestring containers mtl optparse-generic polysemy
-    polysemy-plugin polysemy-zoo servant servant-server text wai
-    wai-cors wai-extra warp
+    aeson base blaze-html bytestring containers mtl optparse-generic
+    polysemy polysemy-plugin polysemy-zoo servant servant-server text
+    wai wai-cors wai-extra warp
   ];
   prePatch = "hpack";
   license = stdenv.lib.licenses.bsd3;
