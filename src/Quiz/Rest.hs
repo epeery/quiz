5ca3cee92c6d1e49cab5b1a493a949acc37d2dc6
Eli Peery <eli.g.peery@gmail.com>
2020-02-01 08:47:32 -0500

    Added endpoint for shareable results picture

--- src/Quiz/Rest.hs
+++ src/Quiz/Rest.hs
@@ -4,20 +4,24 @@ module Quiz.Rest
   )
 where
 
+import Codec.Picture
 import qualified Data.Map.Strict as M
 import Polysemy
 import Quiz
 import Quiz.Candidates
+import Quiz.Effect.Picture
 import Quiz.Effect.Randomize
 import Quiz.Topics
 import Servant
+import Servant.JuicyPixels
 
 type API =
   "api" :> "questions" :> Get '[JSON] [Question]
     :<|> "api" :> "results" :> ReqBody '[JSON] (M.Map Topics Double) :> Post '[JSON] Results
+    :<|> "api" :> "image" :> ReqBody '[JSON] Results :> Post '[PNG] DynamicImage
 
 api :: Proxy API
 api = Proxy
 
-server :: (Member Randomize r) => ServerT API (Sem r)
-server = getQuestions :<|> matchUser
+server :: (Members '[Randomize, Picture] r) => ServerT API (Sem r)
+server = getQuestions :<|> matchUser :<|> getPicture
