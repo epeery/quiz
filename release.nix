c4f3f5dbc59992eab42557067aae156986e0fe75
Eli Peery <eli.g.peery@gmail.com>
2020-02-27 15:54:58 -0500

    Pinned nixpkgs

--- release.nix
+++ release.nix
@@ -10,7 +10,17 @@ let
     };
   };
 
-  pkgs = import <nixpkgs> { inherit config; };
+  bootstrap = import <nixpkgs> { };
+
+  nixpkgs = builtins.fromJSON (builtins.readFile ./nixpkgs.json);
+
+  src = bootstrap.fetchFromGitHub {
+    owner = "NixOS";
+    repo  = "nixpkgs";
+    inherit (nixpkgs) rev sha256;
+  };
+
+  pkgs = import src { inherit config; };
 
 in
   { app = pkgs.haskellPackages.app; }
