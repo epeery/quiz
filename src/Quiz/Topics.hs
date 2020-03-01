b86a905190442f0a64b7819e0fccb9df181b4f47
Eli Peery <eli.g.peery@gmail.com>
2020-03-01 18:56:51 -0500

    Added back TypeApplications pragma for code formatter

--- src/Quiz/Topics.hs
+++ src/Quiz/Topics.hs
@@ -3,6 +3,7 @@
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeApplications #-}
 
 module Quiz.Topics
   ( Economics (..),
